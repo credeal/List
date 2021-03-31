744ec423b5a43e7d8763b409e3e6e293d6270b42
Fulano <detias@outlook.com>
2021-03-31 20:05:28 -0300

    Começando com Array

--- Collections/Collections/Program.cs
+++ Collections/Collections/Program.cs
@@ -10,6 +10,47 @@ namespace Collections
     {
         static void Main(string[] args)
         {
+            string aulaIntro = "Aula Intro";
+            string aulaModelando = "Aula Modelando";
+            string aulaSets = "Aula Sets";
+
+            //string[] aulas = new string[]
+            //{
+            //    aulaIntro,
+            //    aulaModelando,
+            //    aulaSets
+            //};
+
+            string[] aulas = new string[3];
+            aulas[0] = aulaIntro;
+            aulas[1] = aulaModelando;
+            aulas[2] = aulaSets;
+
+            Imprimir(aulas);
+        }
+
+        public static void MudarTamanhoArray(string[] array)
+        {
+            Array.Resize(ref array, 2);
+        }
+
+        public static void TrocarOrdem(string[] array)
+        {
+            Array.Reverse(array);
+        }
+
+        public static void Buscar(string[] array, string palavra)
+        {
+            Console.WriteLine($"Indice de busca: {Array.IndexOf(array,palavra)}");
+        }
+
+        public static void Imprimir(string[] array)
+        {
+            foreach (var aula in array)
+            {
+                Console.WriteLine(aula);
+            }
         }
     }
 }
+ 
