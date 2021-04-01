f0357f0aa86c84fa7f5ec01ad59de223a04bca43
Fulano <detias@outlook.com>
2021-03-31 23:08:40 -0300

    Lista e Coleção Flexível

--- Collections/Collections/Program.cs
+++ Collections/Collections/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,46 +11,172 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            string aulaIntro = "Aula Intro";
-            string aulaModelando = "Aula Modelando";
-            string aulaSets = "Aula Sets";
-
-            //string[] aulas = new string[]
-            //{
-            //    aulaIntro,
-            //    aulaModelando,
-            //    aulaSets
-            //};
-
-            string[] aulas = new string[3];
-            aulas[0] = aulaIntro;
-            aulas[1] = aulaModelando;
-            aulas[2] = aulaSets;
-
-            Imprimir(aulas);
+            ListaLeitura();
         }
 
-        public static void MudarTamanhoArray(string[] array)
+
+
+        public static void ListaLeitura()
         {
-            Array.Resize(ref array, 2);
+            Curso csharp = new Curso("Marcelos","C# Collections p1");
+            csharp.Adicionar(new Aulas("Listas", 20));
+            csharp.Adicionar(new Aulas("Array", 15));
+            csharp.Adicionar(new Aulas("Dictionary", 30));
+
+            var aulasCopiadas = new List<Aulas>(csharp.Aulas);
+
+            aulasCopiadas.Sort();
+
+            Console.WriteLine("Ordenando: ");
+            foreach (var item in aulasCopiadas)
+                Console.WriteLine(item);
+            
+
+            Console.WriteLine("=========");
+            Console.WriteLine("Sem ordenação: ");
+
+            csharp.ImprimirAulas();
+
+            Console.WriteLine("=========");
+            Console.WriteLine($"Tempo total de cursos: {csharp.Total}");
+
+            Console.WriteLine("=========");
+            Console.WriteLine(csharp);
         }
 
-        public static void TrocarOrdem(string[] array)
+        public static void ListaObj()
         {
-            Array.Reverse(array);
-        }
+            var aula1 = new Aulas("Aula1", 19);
+            var aula2 = new Aulas("Aula2", 25);
+            var aula3 = new Aulas("Aula3", 15);
 
-        public static void Buscar(string[] array, string palavra)
-        {
-            Console.WriteLine($"Indice de busca: {Array.IndexOf(array,palavra)}");
-        }
+            List<Aulas> Aulas = new List<Aulas>() { aula1, aula2, aula3 };
 
-        public static void Imprimir(string[] array)
-        {
-            foreach (var aula in array)
+            Aulas.Sort((first, second) =>
+                first.Tempo.CompareTo(second.Tempo)
+            );
+
+            Aulas.ForEach(aula =>
             {
                 Console.WriteLine(aula);
+            });
+        }
+
+        public static void Lista()
+        {
+            string aula1 = "Aula 01";
+            string aula2 = "Aula 02";
+            string aula3 = "Aula 03";
+
+            //List<string> Aulas = new List<string>()
+            //{
+            //    aula1,
+            //    aula2,
+            //    aula3
+            //};
+
+            List<string> Aulas = new List<string>();
+            Aulas.Add(aula1);
+            Aulas.Add(aula2);
+            Aulas.Add(aula3);
+
+            Aulas.ForEach(aula =>
+            {
+                Console.WriteLine(aula);
+            });
+        }
+
+    }
+
+    class Aulas : IComparable
+    {
+        public string Aula { get; set; }
+        public int Tempo { get; set; }
+
+        public Aulas(string p_aula, int p_tempo)
+        {
+            Aula = p_aula;
+            Tempo = p_tempo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Aula} - {Tempo} minutos";
+        }
+
+        public int CompareTo(object obj)
+        {
+            var aulaObj = obj as Aulas;
+
+            return Tempo.CompareTo(aulaObj.Tempo);
+        }
+    }
+
+    class Curso
+    {
+        private List<Aulas> _aulas;
+        private string _instrutor;
+        private string _nome;
+        private int _total;
+
+        public Curso(string instrutor, string nome)
+        {
+            _aulas = new List<Aulas>();
+            _instrutor = instrutor;
+            _nome = nome;
+        }
+
+        //Protegendo nossa coleção
+        public IList<Aulas> Aulas
+        {
+            get { return new ReadOnlyCollection<Aulas>(_aulas); }
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        public string Instrutor
+        {
+
+            get { return _instrutor; }
+            set { _instrutor = value; }
+        }
+
+        public int Total
+        {
+            get
+            {
+                //int total = 0;
+
+                //_aulas.ForEach(aula => {
+                //    total += aula.Tempo;
+                //});
+
+                //return total;
+
+                return _aulas.Sum(a => a.Tempo);
             }
+        }
+
+        public void Adicionar(Aulas aulas)
+        {
+            _aulas.Add(aulas);
+        }
+
+        public void ImprimirAulas()
+        {
+            _aulas.ForEach(aula =>
+            {
+                Console.WriteLine(aula);
+            });
+        }
+
+        public override string ToString()
+        {
+            return $"Nome do curso: {Nome} - Tempo total: {Total}\nAulas:\n {string.Join("\n ",_aulas)}";
         }
     }
 }
